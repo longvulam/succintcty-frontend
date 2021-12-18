31d86890180f7151b6d33e8b700a77686fb6795d
Lam Long Vu <longvulam91@gmail.com>
2021-12-18 15:06:53 -0800

    Add missing modal

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,47 @@
 import styles from "./Header.module.css";
 import appLogo from "../../media/App_logo.png";
-import { Button } from "@mui/material";
+import { Box, Button, Modal, SxProps, Theme } from "@mui/material";
+import { useState } from "react";
+import LoginForm from "../LoginForm/LoginForm";
+
+const style: SxProps<Theme> = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
 
 const Header = () => {
+  const [loginFormOpen, showLoginForm] = useState(false);
+
   return (
     <div className={styles.header}>
       <img className={styles.logo} src={appLogo} alt="logo of succinct" />
 
-      <Button variant="contained" className={styles.loginFormBtn}>LoginButton</Button>
+      <Button variant="contained"
+        className={styles.loginFormBtn}
+        onClick={() => showLoginForm(true)}
+      >
+        Login
+      </Button>
+
+      <Modal
+        open={loginFormOpen}
+        onClose={() => showLoginForm(false)}
+        aria-labelledby="parent-modal-title"
+        aria-describedby="parent-modal-description"
+      >
+        <Box sx={{ ...style, width: 400 }}>
+          <LoginForm />
+        </Box>
+      </Modal>
     </div>
   )
 }
