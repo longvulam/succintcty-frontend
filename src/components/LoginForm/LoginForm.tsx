d7f6a42d190e81daff5e7587550a4a13f812c7c4
Lam Long Vu <longvulam91@gmail.com>
2021-12-17 23:35:57 -0800

    Add Login component

---
+++ src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,28 @@
+import styles from "./LoginForm.module.css";
+import { useState } from "react";
+
+
+const LoginForm = () => {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  return (
+    <div>
+      <h3>Login</h3>
+      <form action="#" className={styles.form}>
+        <p>
+          <label htmlFor="username">username: </label>
+          <input id="username" type="text" value={username} onChange={e => setUsername(e.currentTarget.value)} />
+        </p>
+        <p>
+          <label htmlFor="password">password: </label>
+          <input id="password" type="password" value={password} onChange={e => setPassword(e.currentTarget.value)} />
+        </p>
+      </form>
+    </div>
+  )
+}
+
+export default LoginForm;
