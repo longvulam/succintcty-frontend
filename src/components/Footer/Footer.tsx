c7f6e71da948895f41a49ece3ae3c38c3294d44e
Blake Michalzik <bmichalzik@gmail.com>
2021-12-18 19:04:09 -0800

    footer and fillert updates

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <p>2021 Team team</p>
+      <p className={styles.footertext}>Succinct - Team team - BCIT Winter Hacks 2021</p>
     </div>
   )
 }
