717dfcadb7d722207031a09efe91131c2a29a769
Blake Michalzik <bmichalzik@gmail.com>
2021-12-18 16:45:10 -0800

    footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <p>2021 Team team</p>
+    </div>
+  )
+}
+
+export default Footer;
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,12 +1,14 @@
 import styles from "./Home.module.css";
 import Summarizer from "../Summarizer/Summarizer";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 const Home = () => {
   return (
     <div className={styles.page}>
       <Header />
       <Summarizer />
+      <Footer />
     </div>
   )
 }
